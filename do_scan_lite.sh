8f683db9752c7258096a869e44ce756fb57490ec
Lizzy <six_eight_squash@yahoo.com>
2024-08-07 16:02:47 -0700

    Implement a 'light' scan of documents, etc ... i.e. with lower JPEG resolution.

---
+++ do_scan_lite.sh
@@ -0,0 +1,59 @@
+#!/bin/sh
+#=============================================================================
+# FILE: do_scan_lite.sh
+#
+# DESCRIPTION:
+#    Uses 'linux' command 'scanimage' to scan documents from
+#    Brother ADS-1200.
+#
+#    Resolution of JPG images set to default, which is 100x100 dpi.
+#=============================================================================
+echo "---> Arguments: $#"
+
+if test "$#" -ne 3; then 
+    echo ""
+    echo "***> Invalid number of parameters"
+    echo ""
+    echo "===> USAGE:"
+    echo "===>    do_scan.sh <file_prefix> <count-start> <single | duplex>"
+    echo "===>"
+    echo "===> Example:"
+    echo "===>    do_scan.sh test 0 single"
+
+    exit 2 
+fi
+
+FILE_NAME_PREFIX=$1
+
+COUNT_START=$2
+
+if test "$3" = "single"; then
+   SOURCE="Automatic Document Feeder(left aligned)"
+elif test "$3" = "duplex"; then
+   SOURCE="Automatic Document Feeder(left aligned,Duplex)"
+else
+   echo "***> Invalid scan option"
+   echo "***> $3 is unrecognized"
+   echo "***> Use 'single' or 'duplex'"
+
+   exit 2
+fi
+
+echo $SOURCE
+
+TO_DIR=`pwd`
+
+#
+# The 'center aligned' option doesn't work ... not sure why.
+# It causes 'seg fault'.
+#
+# --source 'Automatic Document Feeder(center aligned,Duplex)'
+#
+# Only seems to accept 'left aligned'
+#
+
+scanimage -d 'brother5:bus3;dev3' \
+          --source "$SOURCE"      \
+          --format jpeg           \
+          --batch=$TO_DIR/$FILE_NAME_PREFIX%.2d.jpg --batch-start=$COUNT_START
+

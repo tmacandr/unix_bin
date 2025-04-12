81a95d8816fff03a1f222fe2a87bbeb0375f7090
Lizzy <six_eight_squash@yahoo.com>
2025-04-12 14:10:50 -0400

    Convenience script to run 'meld' against left file vs right file or left directory against right directory

---
+++ do_meld.sh
@@ -0,0 +1,47 @@
+#!/bin/sh
+#=============================================================================
+# FILE: do_meld.sh
+#
+# DESCRIPTION:
+#    Uses 'linux' command 'scanimage' to scan documents from
+#    Brother ADS-1200.
+#=============================================================================
+echo "---> Arguments: $#"
+
+if test $# -ne 2; then 
+    echo ""
+    echo "***> Invalid number of parameters"
+    echo ""
+    echo "===> USAGE:"
+    echo "===>    do_meld.sh <file_or_dir_left> <file_or_dir_right>"
+    echo "===>"
+    echo "===> Example:"
+    echo "===>    do_meld.sh readme1.txt readme2.txt"
+
+    exit 1 
+fi
+
+LEFT=$1
+
+RIGHT=$2
+
+ITEMS="$LEFT $RIGHT"
+
+for each in $ITEMS; do
+   if test ! -f $each; then
+      if test ! -d $each; then
+         echo "***> ERROR - item not found"
+         echo "***>       - $each not found"
+         exit 2
+      fi
+   fi
+done
+
+if test "$LEFT" = "$RIGHT"; then
+   echo "***> ERROR - Items are the same"
+   echo "***>       - LEFT..: $LEFT"
+   echo "***>       - RIGHT.: $RIGHT"
+fi
+
+meld $LEFT $RIGHT
+

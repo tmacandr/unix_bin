c34b169cbe9a14f3c5efa2d507f94b9baa879b46
Lizzy <six_eight_squash@yahoo.com>
2025-10-07 23:02:11 -0400

    Getting close:
    1) Replaces blanks with _
    2) Replaces ' with -
    3) Searches each e-mail for the 'Date', extracts it, reverses
       it from "dd mm yyyy" to yyyy_mm_dd
    4) Prepends the date to the 'cleaned' name
    
    Then renames the file.
    
    Seems to work, but is kind of messy.  Capture this, try it out,
    and then (if it works) try to clean it up.
    
    Changes to be committed:
    	modified:   rename_emails.sh

--- rename_emails.sh
+++ rename_emails.sh
@@ -3,11 +3,28 @@
 ls *.eml > TMP_LIST.txt
 
 while IFS= read -r line; do
-  echo "From: \"$line\""
 
-  TO=`echo $line | sed 's/ /_/g'`
+   echo "From: $line"
 
-  echo "To..: $TO"
+   TO=`echo $line | sed 's/ /_/g'`
+
+   TO=`echo $TO | sed "s/'/-/g"`
+
+   #echo "To..: $TO"
+
+   DATE=`cat "$line" | grep "Date: " | sed 's/^.*\.eml://g'`
+
+   DATE=`echo $DATE | awk '{print $5 "_" $4 "_" $3}'`
+
+   echo "Date: $DATE"
+
+   TO=`echo $DATE $TO`
+
+   TO=`echo $TO | sed 's/ /_/g'`
+
+   echo "To..: $TO"
+
+   # mv "$line" $TO
 
 done < "TMP_LIST.txt"
 
